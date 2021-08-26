ef9b8ee93eec1c3196594c56311945da821f5ae6
Cassia Denyelli <cassia.deny@gmail.com>
2021-08-26 17:29:44 -0300

    Add custom texts

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
-`
+`;
+
+const SubTitle = styled.h2`
+  font-size: 25px;
+  color: magenta;
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <>
+      <Title>Página da Cássia</Title>
+      <SubTitle>Meu conteúdo</SubTitle>
+    </>
+  );
 }
