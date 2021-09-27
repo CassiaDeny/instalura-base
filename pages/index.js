d6357531cb06e52e5b65de8bcf625bfc896d89c8
Cassia Denyelli <cassia.deny@gmail.com>
2021-09-27 12:52:37 -0300

    Add footer

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
 import React from "react";
+import Footer from "../src/components/Footer";
 import Menu from "../src/components/Menu";
 
 export default function Home() {
   return (
-    <>
+    <div
+      style={{
+        flex: "1",
+        display: "flex",
+        flexWrap: "wrap",
+        flexDirection: "column",
+        justifyContent: "space-between",
+      }}
+    >
       <Menu></Menu>
-    </>
+      <Footer></Footer>
+    </div>
   );
 }
